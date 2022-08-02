bd631883d11ef594abeb1d132d10e666a8776504
Ali Atashrooz <atashroozarash@gmail.com>
2022-08-02 23:26:50 +0430

    Improve question-2

--- question-2/main.cpp
+++ question-2/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int reverseDigits(int num);
+
 int main()
 {
 	int num;
@@ -9,5 +11,20 @@ int main()
 	cout << "Adad ra vared konid: ";
 	cin >> num;
 
+	num = reverseDigits(num);
+
+	cout << num << endl;
+
 	return 0;
+}
+
+// function for reversing digits of an int
+int reverseDigits(int num)
+{
+    int rev_num = 0;
+    while (num > 0) {
+        rev_num = rev_num * 10 + num % 10;
+        num = num / 10;
+    }
+    return rev_num;
 }
