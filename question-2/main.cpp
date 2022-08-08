49d2ce30226323ea7f45881ef08eea6b2137ad28
Mahan Lamee <mahan.lameie87@gmail.com>
2022-08-08 15:32:04 +0430

    Question 2

--- question-2/main.cpp
+++ question-2/main.cpp
@@ -1,41 +1,84 @@
+#include <algorithm>
 #include <iostream>
 #include <math.h>
-#include <string.h>
-#include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 
-string findSmallestPermutation(string s);
- 
-int main()
+int smallest_larger(int n)
 {
-    // take number input in string
-    string num;
-	cout << "Addad ra vared konid: ";
-	cin >> num;
-
-    cout << "Addade moratab shode: " << findSmallestPermutation(num) << endl;
-
-    return 0;
+    string num = to_string(n);
+    int a_index;
+    int b_index;
+    char largest = 0;
+    cout << endl;
+    for (int i = num.size() - 1; i >= 0; i--) {
+        if (num[i] > largest) {
+            largest = num[i];
+        } else if (num[i] < largest) {
+            a_index = i;
+            char smallest_b = 127;
+            for (int j = i; j < num.size(); j++) {
+                if (num[j] > num[a_index] && num[j] < smallest_b) {
+                    smallest_b = num[j];
+                    b_index = j;
+                }
+            }
+            char temp = num[a_index];
+            num[a_index] = num[b_index];
+            num[b_index] = temp;
+            sort(num.begin() + a_index + 1, num.end());
+            break;
+        }
+    }
+    int result = stoi(num);
+    if (result == n) {
+        return -1;
+    }
+    return result;
 }
 
-// return the smallest number permutation
-string findSmallestPermutation(string s)
+int largest_smaller(int n)
 {
-    int len = s.length();
- 
-    // sort the string
-    sort(s.begin(), s.end());
- 
-    // check for leading zero in string
-    // if there are any leading zeroes,
-    // swap the first zero with first non-zero number
-    int i = 0;
+    string num = to_string(n);
+    int a_index;
+    int b_index;
+    char smallest = 127;
+    cout << endl;
+    for (int i = num.size() - 1; i >= 0; i--) {
+        if (num[i] < smallest) {
+            smallest = num[i];
+        } else if (num[i] > smallest) {
+            a_index = i;
+            char largest_b = 0;
+            for (int j = i; j < num.size(); j++) {
+                if (num[j] < num[a_index] && num[j] > largest_b) {
+                    largest_b = num[j];
+                    b_index = j;
+                }
+            }
+            char temp = num[a_index];
+            num[a_index] = num[b_index];
+            num[b_index] = temp;
+            sort(num.begin() + a_index + 1, num.end(), greater<>());
+            break;
+        }
+    }
+    int result = stoi(num);
+    if (result == n) {
+        return -1;
+    }
+    return result;
+}
 
-    while (s[i] == '0')
-        i++;
-     
-    swap(s[0], s[i]);
-
-    return s;
+int main(int argc, char* argv[])
+{
+    int input;
+    cout << "Adad ra vared konid: ";
+    cin >> input;
+    int smallest = smallest_larger(input);
+    int largest = largest_smaller(input);
+    cout << "A: " << (smallest == -1 ? "impossible" : to_string(smallest)) << endl;
+    cout << "B: " << (largest == -1 ? "impossible" : to_string(largest)) << endl;
+    return 0;
 }
